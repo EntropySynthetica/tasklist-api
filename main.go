8560a302fcf360b672dd63fcc09ecfc133d890fc
Erica Zavaleta <44880722+EntropySynthetica@users.noreply.github.com>
2021-10-05 21:57:30 -0500

    Added the delete method to CORS allowed

--- main.go
+++ main.go
@@ -15,6 +15,6 @@ func main() {
 	fmt.Println("Starting server on the port 8080...")
 
 	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}),
-		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"}),
+		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS", "DELETE"}),
 		handlers.AllowedOrigins([]string{"*"}))(r)))
 }
